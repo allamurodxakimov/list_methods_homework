617807166d6e4b53be7a5dc30f7d92cb0e1436f9
allamurodxakimov <xakimov2806@gmail.com>
2023-10-07 17:24:36 -0700

    list method 8

--- List08.py
+++ List08.py
@@ -6,4 +6,10 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    return
+    a=0
+    while a<len(fruits):
+        if fruits[a]=="apple":
+            fruits.pop(a)
+        a+=1
+    return fruits
+print(main(fruits = ["apple", "banana", "apple", "pear", "apple"]))
