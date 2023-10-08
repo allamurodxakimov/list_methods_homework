cd9165dea2b0509bbc4f4682316d8b80b6a24eb3
allamurodxakimov <xakimov2806@gmail.com>
2023-10-07 17:23:48 -0700

    list method 5

--- List05.py
+++ List05.py
@@ -9,4 +9,8 @@ def main(numbers1, numbers2):
     Returns:
         list: return answer
     """
-    return
+    k=numbers1.pop(-1)
+    numbers2.insert(0,k)
+    numbers1.extend(numbers2)
+    return numbers1
+print(main([1,2,3,4,5],[9,8,7,6]))
