c47658b1c8bbc3b5e49d5c3cba82c820212f8c38
allamurodxakimov <xakimov2806@gmail.com>
2023-10-07 17:25:12 -0700

    list method 10

--- List10.py
+++ List10.py
@@ -6,4 +6,10 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    ls=[]
+    k=list1.count(1)
+    l=list1.count(0)
+    ls.append(k)
+    ls.append(l)
+    return ls
+print(main(list1 = [0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1]))
